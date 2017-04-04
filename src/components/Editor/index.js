d5deb836368c8e3146d73abd033644b1f6a2c59a
Will Leach <willleach@Wills-MacBook-Air.local>
2017-04-04 21:59:45 +1000

    implemented draftjs-plugins

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,41 +1,35 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Editor, EditorState, RichUtils, getVisibleSelectionRect } from 'draft-js';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
 import 'draft-js/dist/Draft.css';
+import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 import styles from './styles.scss';
-import { getSelection, getSelectionRect } from './utils/selection';
-import Toolbar from './Toolbar';
+
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+
+console.log(inlineToolbarPlugin);
 
 export default class CustomerEditor extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      titleEditorState: EditorState.createEmpty(),
-      editorState: EditorState.createEmpty(),
-      toolbarOpen: false,
-      selectionRect: null,
-      hasFocus: false
+      titleEditorState: createEditorStateWithText(''),
+      editorState: createEditorStateWithText(''),
     };
 
     this.titleEditorOnChange = (titleEditorState) => this.setState({ titleEditorState });
     this.onChange = (editorState) => this.setState({ editorState });
   }
 
-  onFocus() {
-    this.setState({
-      hasFocus: true
-    });
+  focusTitleEditor() {
+    this.titleEditor.focus();
   }
 
-  onBlur() {
-    this.setState({
-      hasFocus: false
-    });
-  }
-
-  toggleInlineStyle(style) {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, style));
+  focusMainEditor() {
+    this.mainEditor.focus();
   }
 
   render() {
@@ -44,53 +38,23 @@ export default class CustomerEditor extends Component {
 
     return (
       <div className={classnames(styles.editor, className)}>
-        <div className={styles.titleEditor}>
+        <div className={styles.titleEditor} onClick={this.focusTitleEditor.bind(this)}>
           <Editor
+            ref={(element) => this.titleEditor = element}
             editorState={titleEditorState}
             onChange={this.titleEditorOnChange}
             placeholder="Title"/>
         </div>
-        <Toolbar 
-          {...getToolbarProps(this.state)}
-          toggleInlineStyle={this.toggleInlineStyle.bind(this)}/>
-        <Editor
-          onBlur={this.onBlur.bind(this)}
-          onFocus={this.onFocus.bind(this)}
-          placeholder="Write here..."
-          editorState={editorState}
-          onChange={this.onChange}/>
+        <div className={styles.mainEditor} onClick={this.focusMainEditor.bind(this)}>
+          <Editor
+            ref={(element) => this.mainEditor = element}
+            plugins={[inlineToolbarPlugin]}
+            placeholder="Write here..."
+            editorState={editorState}
+            onChange={this.onChange}/>
+        </div>
+        <InlineToolbar/>          
       </div>
     );
   }
-}
-
-function getToolbarProps({ editorState, hasFocus }) {
-  const selectionState = editorState.getSelection();
-
-  const toolbarProps = {
-    open: false,
-    position: {}
-  };
-
-  const isCollapsed = selectionState.isCollapsed();
-  const start = selectionState.getStartOffset();
-  const end = selectionState.getEndOffset();
-
-  if(hasFocus && !isCollapsed) {
-    const nativeSelection = getSelection(window);
-    const selectionRect = getSelectionRect(nativeSelection);
-
-    // sometimes selection rect has no rangecount
-    // even though there is collapse, was getting an error, see:
-    // http://stackoverflow.com/questions/22935320/uncaught-indexsizeerror-failed-to-execute-getrangeat-on-selection-0-is-not
-    if (selectionRect) {
-      const { left, right, top, bottom } = selectionRect;
-
-      toolbarProps.position.left = left + ((right - left) / 2);
-      toolbarProps.position.top = top;
-      toolbarProps.open = true;
-    }
-  }
-
-  return toolbarProps;
 }
