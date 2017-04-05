1948a2d1f821c0ab5d7919f6da0e0a252446d269
William Leach <Will@ip-10-101-23-199.ap-southeast-2.compute.internal>
2017-04-05 17:14:00 +1000

    update reducer

---
+++ src/reducers/notes.js
@@ -0,0 +1,19 @@
+const initialState = {
+  notes: {},
+  ids: []
+};
+
+export default function notes(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_NOTE':
+      return Object.assign({}, state, {
+        notes: Object.assign({}, state.notes, {
+          [state.ids.length]: action.note
+        }),
+        ids: state.ids.concat[state.ids.length],
+        activeNote: state.ids.length
+      });
+    default:
+      return state;
+  }
+}
